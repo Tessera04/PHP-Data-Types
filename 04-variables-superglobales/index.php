c9ac5f677092732182713735325955372b7b2655
Matias <matute_11@hotmail.com.ar>
2023-10-12 02:21:52 -0300

    Variables superglobales

---
+++ 04-variables-superglobales/index.php
@@ -0,0 +1,9 @@
+$GLOBALS
+$_SERVER <?php /*Array que contiene informacion como cabeceras, rutas y ubicaciones de script. Depende del servidor, algunos pueden omitir algunos campos*/  ?>
+$_GET <?php /*Array que contiene los datos que pasan por la URL*/  ?>
+$_POST <?php /*Array que contiene los datos que pasan a traves de un request de tipo POST (Normalmente un formulario)*/  ?>
+$_FILES <?php /*Array asociativo de archivos subidos a traves de un formulario con metodo POST*/  ?>
+$_COOKIE <?php /*Array de Cookies del usuario que visita el sitio*/  ?>
+$_SESSION <?php /*Array donde se puede guardar informacion sobre un usuario. Al recargar, dicha informacion no se pierde*/  ?>
+$_REQUEST <?php /*Contiene $_GET, $_POST y $_COOKIE*/  ?>
+$_ENV
